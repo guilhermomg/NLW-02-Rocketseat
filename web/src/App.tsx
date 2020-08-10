23f45ae705ae376a0b8d2ab374fa7a85644aa5bb
ggonzalez <ggonzalez@tatico.co>
2020-08-10 09:45:44 -0300

    Web files

---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./assets/styles/global.css";
+import Routes from "./routes";
+
+//  JSX - Javascript + XML (ou HTML dentro do Javascript)
+
+function App() {
+  return <Routes />;
+}
+
+export default App;
